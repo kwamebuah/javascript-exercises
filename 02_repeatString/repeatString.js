49389dc711088701eca0ff16051bbd542898f2fc
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2024-12-31 15:42:08 +0000

    Completed exercise and enabled tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = '';
+    let errorMessage = 'ERROR';
+    if (num < 0) {
+        return errorMessage;
+    }
+    else {
+        for (let i = 0; i < num; i++) {
+            newString += string; 
+        }
+        return newString;
+    }
 };
 
 // Do not edit below this line
