28ced1b38c01913915ce2cb3d61e6037beae980d
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-01-01 20:10:41 +0000

    Provided solution for exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const leapYear = true;
+    const notLeapYear = false;
+    
+    if ((year % 4 === 0) && (year % 100 !==0)) return leapYear;
+    else if (year % 400 === 0) return leapYear;
+    else return notLeapYear;
 };
 
 // Do not edit below this line
