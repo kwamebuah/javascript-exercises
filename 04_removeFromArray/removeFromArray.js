7de514caf9854e2bbd7d4352c9fcfb1cec55c834
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-01-01 16:15:05 +0000

    Provided solution to exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    let returnArr = [];
+    for (let i = 0; i < toRemove.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] === toRemove[i]) {
+                delete arr[j];
+            }
+        }  
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== undefined) {
+            returnArr.push(arr[i]);
+        }
+    }
+    return returnArr;
 };
 
 // Do not edit below this line
