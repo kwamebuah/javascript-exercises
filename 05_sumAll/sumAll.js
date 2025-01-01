e84fb1810bcb7ae0a18cd7bfdb0e34decf0844e9
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-01-01 18:52:31 +0000

    Provided solution to exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let errorMessage = 'ERROR';
+    if (a < 0 || b < 0) {
+        return errorMessage;
+    }
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return errorMessage;
+    }
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    let length = b - a;
+    let sum = a;
 
+    for (let i = 1; i <= length; i++) {
+        sum += a + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
