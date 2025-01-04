80baf0458550b443b839dd290f845d367d997fca
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-01-04 08:44:41 +0000

    Provided an alternative solution to the exercise
    Started the iteration on the array from the end to eliminate the problem
    of Array.splice() changing the position of the elements within the
    array during iteration.
    The previous solution used delete to address the problem then reform the
    new array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,29 @@
+// const removeFromArray = function(arr, ...toRemove) {
+//     let returnArr = [];
+//     for (let i = 0; i < toRemove.length; i++) {
+//         for (let j = 0; j < arr.length; j++) {
+//             if (arr[j] === toRemove[i]) {
+//                 delete arr[j];
+//             }
+//         }  
+//     }
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] !== undefined) {
+//             returnArr.push(arr[i]);
+//         }
+//     }
+//     return returnArr;
+// };
+
 const removeFromArray = function(arr, ...toRemove) {
-    let returnArr = [];
     for (let i = 0; i < toRemove.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
+        for (let j = arr.length - 1; j >= 0; j--) {
             if (arr[j] === toRemove[i]) {
-                delete arr[j];
+                arr.splice(j, 1);
             }
         }  
     }
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== undefined) {
-            returnArr.push(arr[i]);
-        }
-    }
-    return returnArr;
+    return arr;
 };
 
 // Do not edit below this line
