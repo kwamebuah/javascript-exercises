676294ab1a79d73d32e085d613c8b6f539caf31c
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-01-04 08:51:13 +0000

    Removed previous code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,27 +1,10 @@
-// const removeFromArray = function(arr, ...toRemove) {
-//     let returnArr = [];
-//     for (let i = 0; i < toRemove.length; i++) {
-//         for (let j = 0; j < arr.length; j++) {
-//             if (arr[j] === toRemove[i]) {
-//                 delete arr[j];
-//             }
-//         }  
-//     }
-//     for (let i = 0; i < arr.length; i++) {
-//         if (arr[i] !== undefined) {
-//             returnArr.push(arr[i]);
-//         }
-//     }
-//     return returnArr;
-// };
-
 const removeFromArray = function(arr, ...toRemove) {
     for (let i = 0; i < toRemove.length; i++) {
         for (let j = arr.length - 1; j >= 0; j--) {
             if (arr[j] === toRemove[i]) {
                 arr.splice(j, 1);
             }
-        }  
+        }
     }
     return arr;
 };
