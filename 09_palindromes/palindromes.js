bf341145c991912686ef999cbd7de50dd353356c
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-01-19 18:14:26 +0000

    Provide solution to exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newStr = (" " + str).slice(1);
+    newStr = newStr.split('').filter(char => /[a-zA-Z0-9]/.test(char)).join('').toLocaleLowerCase();
+    let rev = "";
+    for (let i = newStr.length - 1; i >= 0; i--) {
+        rev = rev + newStr.charAt(i);
+    }
 
+    return newStr === rev ? true : false;
 };
 
 // Do not edit below this line
