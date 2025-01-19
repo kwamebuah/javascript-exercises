451504e6bfc4c9812740c5112dfcc70e8113e920
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-01-19 20:12:27 +0000

    Provide solution to exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    if (number < 0) return 'OOPS';
+    let fib = [0, 1];
+    for (let i = 1; i < number; i++) {
+        fib.push(fib[i - 1] + fib[i]);
+    }
+    return fib[number];
 };
 
 // Do not edit below this line
