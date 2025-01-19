7843461d139e8f5c67855888bcea720b01867d6d
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-01-19 22:39:40 +0000

    Provide solution to exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const oldest = people.sort((person1, person2) => {
+        if (!person1.yearOfDeath) {
+            person1.yearOfDeath = (new Date()).getFullYear();
+        }
+        if (!person2.yearOfDeath) {
+            person2.yearOfDeath = (new Date()).getFullYear();
+        }
+        return (person2.yearOfDeath - person2.yearOfBirth) - (person1.yearOfDeath - person1.yearOfBirth);
+    })[0];
 
+    return oldest;
 };
 
 // Do not edit below this line
